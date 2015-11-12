dea0518f0629586714d880706f28e6c6447b82ca
bug2bug <S.Sobko@profitware.ru>
2015-11-12 23:08:34 +0300

    Flatten: Pass tests

--- src/clojure_sandbox/core.clj
+++ src/clojure_sandbox/core.clj
@@ -7,4 +7,4 @@
   []
   (println "Middle point for 1 and 2 is" (bs/midpoint 1 2))
   (println "Binsearch [1 2 3 4 6 8 9 11] 4 is" (bs/binsearch [1 2 3 4 6 8 9 11] 4))
-  (println "Flatten ((1 (2)) 3) is" (f/flatten-list '((1 (2)) 3))))
+  (println "Flatten (((1 (2)) 3)) (4) is" (f/flatten-list '(((1 (2)) 3)) '(4))))
--- src/clojure_sandbox/flatten.clj
+++ src/clojure_sandbox/flatten.clj
@@ -2,16 +2,16 @@
   (:require [clojure.walk :as w])
   (:gen-class))
 
-(defn flatten-list [args]
-  (def accumulator (list))
-  (defn flatten-list-inner
-    ([args-inner]
-      (if (list? args-inner)
-        (do
-          (def x (w/walk flatten-list-inner concat args-inner))
-          x)
-        (do
-          (def accumulator (cons args-inner accumulator))
-          args-inner))))
-  (flatten-list-inner args)
-  (reverse accumulator))
+(defn flatten-list 
+  ([]
+    (list))
+  
+  ([& args]
+    (def accumulator (list))
+    (defn flatten-list-inner
+      ([args-inner]
+        (if (seq? args-inner)
+          (w/walk flatten-list-inner concat args-inner)
+          (def accumulator (cons args-inner accumulator)))))
+    (flatten-list-inner args)
+    (reverse accumulator)))
