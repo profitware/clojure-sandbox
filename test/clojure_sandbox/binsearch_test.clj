fff18355c74c6345fcb54caeffc7777fde9a91d8
bug2bug <S.Sobko@profitware.ru>
2015-11-14 16:29:35 +0300

    Flatten and Binary search: Rewrite tests

--- test/clojure_sandbox/binsearch_test.clj
+++ test/clojure_sandbox/binsearch_test.clj
@@ -3,45 +3,33 @@
             [clojure-sandbox.binsearch :as bs]))
 
 (deftest midpoint-test
-  (testing "zero zero"
-    (is (= 0 (bs/midpoint 0 0))))
+  (testing "equal"
+           (are [x y] (= x y)
+                0 (bs/midpoint 0 0)
+                1 (bs/midpoint 1 1)))
 
-  (testing "zero one"
-    (is (= 0 (bs/midpoint 0 1))))
-
-  (testing "zero two"
-    (is (= 1 (bs/midpoint 0 2))))
-
-  (testing "one one"
-    (is (= 1 (bs/midpoint 1 1))))
-
-  (testing "one two"
-    (is (= 1 (bs/midpoint 1 2))))
-
-  (testing "one five"
-    (is (= 3 (bs/midpoint 1 5)))))
+  (testing "neighbors"
+           (are [x y] (= x y)
+                0 (bs/midpoint 0 1)
+                1 (bs/midpoint 1 2)
+                2 (bs/midpoint 1 4)))
+  
+  (testing "middle"
+           (are [x y] (= x y)
+                1 (bs/midpoint 0 2)
+                3 (bs/midpoint 1 5))))
 
 (deftest binsearch-test
-  (testing "not found test1"
-    (is (nil? (bs/binsearch []  1))))
-  
-  (testing "binsearch test1"
-    (is (= 2 (bs/binsearch [1 3 4 6 8 9 11] 4))))
-  
-  (testing "binsearch test2"
-    (is (= 3 (bs/binsearch [1 2 3 4 6 8 9 11] 4))))
+  (testing "not found"
+           (are [x] (nil? x)
+                (bs/binsearch [] 1)
+                (bs/binsearch [1 2 3] 4)
+                (bs/binsearch [-1 2 4] 0)))
 
-  (testing "binsearch test3"
-    (is (= 0 (bs/binsearch [1 2 3 4 6 8 9 11] 1))))
-
-  (testing "binsearch test4"
-    (is (= 7 (bs/binsearch [1 2 3 4 6 8 9 11] 11))))
-
-  (testing "binsearch test5"
-    (is (= 6 (bs/binsearch [1 2 3 4 8 9 11] 11))))
-  
-  (testing "not found test2"
-    (is (nil? (bs/binsearch [1 2 3] 4))))
-  
-  (testing "not found test3"
-    (is (nil? (bs/binsearch [-1 2 4] 0)))))
+  (testing "binsearch"
+           (are [x y] (= x y)
+                2 (bs/binsearch [1 3 4 6 8 9 11] 4)
+                3 (bs/binsearch [1 2 3 4 6 8 9 11] 4)
+                0 (bs/binsearch [1 2 3 4 6 8 9 11] 1)
+                7 (bs/binsearch [1 2 3 4 6 8 9 11] 11)
+                6 (bs/binsearch [1 2 3 4 8 9 11] 11))))
--- test/clojure_sandbox/flatten_test.clj
+++ test/clojure_sandbox/flatten_test.clj
@@ -4,25 +4,16 @@
 
 (deftest flatten-test
   (testing "empty list"
-    (is (= (list) (f/flatten-list))))
+           (is (= (list) (f/flatten-list))))
   
   (testing "one element list"
-    (is (= '(1) (f/flatten-list 1))))
+           (is (= '(1) (f/flatten-list 1))))
 
-  (testing "three elements test0"
-    (is (= '(1 2 3) (f/flatten-list 1 2 3))))
-  
-  (testing "three elements test1"
-    (is (= '(1 2 3) (f/flatten-list '(1) 2 3))))
-
-  (testing "three elements test2"
-    (is (= '(1 2 3) (f/flatten-list '(1) '(2 3)))))
-
-  (testing "three elements test3"
-    (is (= '(1 2 3) (f/flatten-list '(1 2 3)))))
-
-  (testing "three elements test4"
-    (is (= '(1 2 3) (f/flatten-list '(1 (2 3))))))
-
-  (testing "three elements test5"
-    (is (= '(1 2 3) (f/flatten-list '((1 2 3)))))))
+  (testing "three elements list"
+           (are [x y] (= x y)
+                '(1 2 3) (f/flatten-list 1 2 3)
+                '(1 2 3) (f/flatten-list '(1) 2 3)
+                '(1 2 3) (f/flatten-list '(1) '(2 3))
+                '(1 2 3) (f/flatten-list '(1 2 3))
+                '(1 2 3) (f/flatten-list '(1 (2 3)))
+                '(1 2 3) (f/flatten-list '((1 2 3))))))
