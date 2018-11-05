333c4c9f3a1ca82cf733d32ae4248884654af313
Sergey Sobko <S.Sobko@profitware.ru>
2018-11-05 21:09:15 +0300

    CTfP - Chapter 4 - Kleisli Categories

--- src/clojure_sandbox/category_theory_for_programmers.clj
+++ src/clojure_sandbox/category_theory_for_programmers.clj
@@ -74,3 +74,89 @@
                                          (->Monoid (comp #(mod % divisor) +)
                                                    divisor)))
 (def numeric-add-modulo3-monoid (numeric-add-modulo-monoid-factory 3))
+
+
+;; Chapter 4
+
+(defprotocol KleisliProto
+  (kidentity [kleisli x])
+  (kcompose [kleisli f g]))
+
+
+(defrecord WriterMonad [writer-monoid]
+  KleisliProto
+  (kidentity [kleisli x] [x (.mempty writer-monoid)])
+  (kcompose [kleisli f g] (fn [x]
+                            (let [[r1 w1] (g x)
+                                  [r2 w2] (f r1)]
+                              [r2 (.mappend writer-monoid w1 w2)]))))
+
+
+(def string-writer (->WriterMonad string-monoid))
+
+
+(defn >=> [& args]
+  (reduce (fn [acc kfunction]
+            (.kcompose string-writer kfunction acc))
+          #(.kidentity string-writer %)
+          args))
+
+
+(defn k-up-case [x]
+  [(.toUpperCase x) "k-up-case "])
+
+
+(defn k-words [x]
+  [(clojure.string/split x #" ") "k-words "])
+
+
+(defn k-process [x]
+  (apply (>=> k-up-case k-words)
+         [x]))
+
+
+(defprotocol OptionalProto
+  (is-valid [optional])
+  (value [optional]))
+
+
+(defrecord Optional [value is-valid]
+  OptionalProto
+  (is-valid [optional] is-valid)
+  (value [optional] value))
+
+
+(defrecord OptionalMonad []
+  KleisliProto
+  (kidentity [kleisli x] (->Optional x true))
+  (kcompose [kleisli f g] (fn [x]
+                            (let [o1 (g x)]
+                              (if (.is-valid o1)
+                                (f (.value o1))
+                                (->Optional nil false))))))
+
+
+(def optional (->OptionalMonad))
+(def optional-non-valid (->Optional nil false))
+
+
+(defn safe-root [x]
+  (if (> x 0)
+    (.kidentity optional (Math/sqrt x))
+    optional-non-valid))
+
+
+(defn safe-reciprocal [x]
+  (if (= x 0)
+    optional-non-valid
+    (.kidentity optional (/ 1 x))))
+
+
+(defn safe-root-reciprocal [x]
+  (apply (.kcompose optional safe-root safe-reciprocal) [x]))
+
+
+(deftest safe-root-reciprocal-test
+  (is (= optional-non-valid (safe-root-reciprocal -1)))
+  (is (= optional-non-valid (safe-root-reciprocal 0)))
+  (is (= 1.0 (.value (safe-root-reciprocal 1)))))
